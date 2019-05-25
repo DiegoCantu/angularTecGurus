1351cd664955058a882fe1c8ebbe1cebb2e95711
DiegoCantu <diegotkd2@hotmail.com>
2019-05-25 12:18:40 -0500

    modulos internos modulos npm

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {SharedModule} from './shared/shared.module';
+import { ListaComponent } from './pages/lista/lista.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListaComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
